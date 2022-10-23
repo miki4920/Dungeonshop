c0675af89de8bb0243f00743364456df1e613af7
miki4920 <miki4920@gmail.com>
2022-10-23 20:30:06 +0100

    Added deletion and layer changing to the layer manager

--- Assets/DungeonshopScripts/UI/Layers/LayerManagerViewController.cs
+++ Assets/DungeonshopScripts/UI/Layers/LayerManagerViewController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Dungeonshop;
 
 namespace Dungeonshop.UI
@@ -13,21 +14,35 @@ namespace Dungeonshop.UI
         
         public void addLayer()
         {
-            Dungeonshop.LayerManager.Instance.createNewLayer();
+            Layer blankLayer = Dungeonshop.LayerManager.Instance.createNewLayer();
             GameObject layerContainer = Instantiate(this.layerContainerPrefab, this.layersContainer);
+            layerContainer.GetComponent<LayerView>().layer = blankLayer;
+            layerContainer.GetComponent<LayerView>().viewController = this;
+            layerContainer.transform.GetChild(1).GetComponent<RawImage>().texture = blankLayer.background;
             this.layerContainers.Insert(Dungeonshop.LayerManager.Instance.layer, layerContainer);
         }
 
-        public void removeLayer()
+        public void changeLayer(Layer layer)
         {
-            Destroy(layerContainers[Dungeonshop.LayerManager.Instance.layer]);
-            layerContainers.RemoveAt(Dungeonshop.LayerManager.Instance.layer);
-            Dungeonshop.LayerManager.Instance.deleteCurrentLayer();
+            for (int i = 0; i < Dungeonshop.LayerManager.Instance.layers.Count; i++)
+            {
+                if (layer == Dungeonshop.LayerManager.Instance.layers[i]) {
+                    Dungeonshop.LayerManager.Instance.layer = i;
+                }
+            }
         }
 
-        public void Update()
+        public void deleteLayer(Layer layer)
         {
-            
+            for (int i = 0; i < Dungeonshop.LayerManager.Instance.layers.Count; i++)
+            {
+                if (layer == Dungeonshop.LayerManager.Instance.layers[i])
+                {
+                    Dungeonshop.LayerManager.Instance.deleteLayer(i);
+                    Destroy(layerContainers[i]);
+                    layerContainers.RemoveAt(i);
+                }
+            }
         }
     }
 }
