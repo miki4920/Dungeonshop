4dba78d7202f66bff857b8fb87d13d6dfa7af503
miki4920 <miki4920@gmail.com>
2023-01-15 20:03:48 +0000

    Removed saving of the mouse position within background manager

--- Assets/DungeonshopScripts/Drawing/BackgroundManager.cs
+++ Assets/DungeonshopScripts/Drawing/BackgroundManager.cs
@@ -46,7 +46,6 @@ namespace Dungeonshop
             displayLayer = createBlankRenderTexture();
             maskLayer = createBlankRenderTexture();
             canvas.texture = canvasLayer;
-            previousMousePosition = Input.mousePosition;
         }
 
 
