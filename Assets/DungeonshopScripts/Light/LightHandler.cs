1e04ed66a3ffd622113ecbb76481d0779729359f
miki4920 <miki4920@gmail.com>
2023-01-31 14:28:24 +0000

    Created rotatable lights

--- Assets/DungeonshopScripts/Light/LightHandler.cs
+++ Assets/DungeonshopScripts/Light/LightHandler.cs
@@ -1,10 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using static UnityEngine.Rendering.DebugUI;
+
 namespace Dungeonshop.UI
 {
+    public enum LightMode
+    {
+        Environment,
+        Light
+    }
     public class LightHandler : ColorReceiver
     {
         public static LightHandler LightInstance;
@@ -20,6 +25,7 @@ namespace Dungeonshop.UI
         [SerializeField] SliderLayout angleSlider;
         float angle;
         Color color;
+        [HideInInspector] public LightMode lightMode;
         bool restoreValues;
         [HideInInspector] public GameObject lightInstance;
         FieldInfo m_FalloffField = typeof(Light2D).GetField("m_FalloffIntensity", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -41,6 +47,11 @@ namespace Dungeonshop.UI
             this.color = color;
         }
 
+        public void changeMode(int newMode)
+        {
+            lightMode = (LightMode) newMode;
+        }
+
         public void createLight(Vector3 position)
         {
             lightInstance = new GameObject();
@@ -55,17 +66,25 @@ namespace Dungeonshop.UI
             lightInstance.transform.position = position;
         }
 
+        public void updateRotation(float mouseDelta)
+        {
+            mouseDelta = mouseDelta >= 0 ? 5 : -5;
+            lightInstance.transform.Rotate(new Vector3(0, 0, mouseDelta));
+        }
+
         public void updateLight(GameObject light)
         {
             Light2D lightComponent = light.GetComponent<Light2D>();
             lightComponent.pointLightOuterRadius = radiusOuterSlider.currentValue * 256;
-            lightComponent.pointLightInnerRadius = radiusInnerSlider.currentValue * 256;
+            lightComponent.pointLightInnerRadius = Mathf.Min(radiusInnerSlider.currentValue, radiusOuterSlider.currentValue) * 256;
             lightComponent.intensity = intensitySlider.currentValue;
             // Needs to be done via reflection because setter doesn't exist
             m_FalloffField.SetValue(lightComponent, falloffStrengthSlider.currentValue);
             lightComponent.color = color;
             lightComponent.blendStyleIndex = 0;
             lightComponent.overlapOperation = Light2D.OverlapOperation.Additive;
+            lightComponent.pointLightInnerAngle = angle;
+            lightComponent.pointLightOuterAngle = angle;
 
     }
 
