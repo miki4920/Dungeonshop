df970bd40ad988263aec24f3c1dab7f222156541
miki4920 <miki4920@gmail.com>
2023-02-06 21:16:21 +0000

    Added a selection manager that handles anything to do with selection

--- Assets/DungeonshopScripts/Drawing/SelectionManager.cs
+++ Assets/DungeonshopScripts/Drawing/SelectionManager.cs
@@ -1,16 +1,75 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public class SelectionManager : MonoBehaviour
+namespace Dungeonshop.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum SelectionMode
     {
-        
+        Light,
+        Wall,
+        None
+    }
+    public class SelectionManager : MonoBehaviour
+    {
+        public static SelectionManager Instance;
+        public bool isObjectSet;
+        public GameObject selectedObject;
+        public GameObject lightPanel;
+        public SelectionMode mode;
+        public LightHandler lightHandler;
+        bool setObjectValues;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+            mode = SelectionMode.None;
+        }
+
+        public void setSelection(GameObject selection)
+        {
+            selectedObject = selection;
+            isObjectSet = true;
+            setObjectValues = false;
+        }
+
+        public void disableTabs()
+        {
+            lightHandler.gameObject.SetActive(false);
+        }
+
+        public void updateSelection()
+        {
+            if (selectedObject == null)
+            {
+                disableTabs();
+            }
+            else if(selectedObject != null)
+            {
+                if(selectedObject.GetComponent<Light2D>() != null)
+                {
+                    if(!setObjectValues)
+                    {
+                        lightHandler.gameObject.SetActive(true);
+                        lightHandler.lightInstance = selectedObject;
+                        lightHandler.updateSliders();
+                        mode = SelectionMode.Light;
+                        setObjectValues = true;
+                    }
+                    lightHandler.updateLight();
+                    
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
+
