4d851f5d8404610b48b4e0ee1ff25953d7d6db16
miki4920 <miki4920@gmail.com>
2023-02-19 23:39:53 +0000

    Updated load textures to allow for loading of walls

--- Assets/DungeonshopScripts/FileHandling/LoadTextures.cs
+++ Assets/DungeonshopScripts/FileHandling/LoadTextures.cs
@@ -1,29 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Dungeonshop.UI;
 using TMPro;
+using UnityEditor;
 
-namespace Dungeonshop.UI
+namespace Dungeonshop
 {
     public class LoadTextures : MonoBehaviour
     {
         [SerializeField] private string path;
         [SerializeField] GameObject textureTilePrefab;
-        public List<GameObject> textures = new List<GameObject>();
+        [SerializeField] GameObject textureReceiver;
+        List<GameObject> textures = new List<GameObject>();
+        public bool sprite;
         void Start()
         {
             Object[] loadedTextures = Resources.LoadAll(path);
             foreach (Object texture in loadedTextures)
             {
                 GameObject temporaryTile = Instantiate(textureTilePrefab, gameObject.transform);
-                temporaryTile.transform.GetChild(0).GetComponent<RawImage>().texture = (Texture2D)texture;
+                temporaryTile.GetComponent<TextureView>().updateTexture((Texture2D) texture);
+                temporaryTile.GetComponent<TextureView>().textureReceiver = textureReceiver.GetComponent<TextureReceiver>();
                 temporaryTile.GetComponent<TabButton>().tabName = texture.name;
-                temporaryTile.GetComponent<TabButton>().tabGroup = gameObject.GetComponent<TabGroup>();
+                temporaryTile.GetComponent<TabButton>().tabGroup = gameObject.GetComponent<TabGroup>(); 
                 textures.Add(temporaryTile);
             }
-            BrushSelectorManager.Instance.updateTexture((Texture2D) textures[0].transform.GetChild(0).GetComponent<RawImage>().texture);
+            textureReceiver.GetComponent<TextureReceiver>().updateTexture((Texture2D)textures[0].transform.GetChild(0).GetComponent<RawImage>().texture);
+
         }
 
         public void updateTextureList(GameObject textureSearchBar)
